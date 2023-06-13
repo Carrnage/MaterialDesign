665b9dec5c6fc3520b720e745eae350ece5895ee
Carrnage <Carrnage4@gmail.com>
2023-06-13 16:16:20 +1200

    Initial commit

---
+++ app/src/main/java/com/quintincarr/materialdesign/HotelFragment.java
@@ -0,0 +1,29 @@
+package com.quintincarr.materialdesign;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class HotelFragment extends ListFragment {
+    public HotelFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.hotels));
+        this.setListAdapter(adapter);
+
+
+        return inflater.inflate(R.layout.fragment_hotel, container, false);
+    }
+}
